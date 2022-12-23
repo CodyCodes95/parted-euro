dbdca31aede55984125bedeb67d6c6fdcb30f923
Cody Partington <codythatsme@gmail.com>
2022-12-23 23:57:45 +1100

    started writing car router

--- src/components/modals/AddCar.tsx
+++ src/components/modals/AddCar.tsx
@@ -13,6 +13,10 @@ const AddCar: React.FC<AddCarProps> = ({ showModal, setShowModal }) => {
   const [generation, setGeneration] = React.useState<string>("");
   const [model, setModel] = React.useState<string>("");
 
+  const saveCar = async () => {
+    
+  }
+
   return (
     <div
       id="defaultModal"
@@ -68,18 +72,12 @@ const AddCar: React.FC<AddCarProps> = ({ showModal, setShowModal }) => {
           </div>
           <div className="flex items-center space-x-2 rounded-b border-t border-gray-200 p-6 dark:border-gray-600">
             <button
+              onClick={saveCar}
               data-modal-toggle="defaultModal"
               type="button"
               className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Save car
-            </button>
-            <button
-              data-modal-toggle="defaultModal"
-              type="button"
-              className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-600"
-            >
-              Cancel
             </button>
           </div>
         </div>
