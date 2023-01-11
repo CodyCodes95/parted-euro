b7cac847b8ad5c04c513dd9a6c6d8b4eeeab223e
Cody Partington <codythatsme@gmail.com>
2023-01-12 00:59:48 +1100

    added key for cart items

--- src/components/Nav/CartPopup.tsx
+++ src/components/Nav/CartPopup.tsx
@@ -71,7 +71,7 @@ const CartPopup: React.FC<CartPopupProps> = ({ showCart, setShowCart }) => {
         <div className="flow-root">
           <ul className="-my-8">
             {cart.map((item: CartItem) => (
-              <li className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+              <li key={item.listingId} className="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
                 <div className="shrink-0">
                   <img
                     className="h-24 w-24 max-w-full rounded-lg object-cover"
