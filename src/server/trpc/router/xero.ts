a5c96c3d994b75cd31a4b5155e906fdcbd114388
Cody Partington <codythatsme@gmail.com>
2023-01-21 17:06:30 +1100

    further dev of xero create invoice api

--- src/server/trpc/router/xero.ts
+++ src/server/trpc/router/xero.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { router, adminProcedure } from "../trpc";
-import { XeroClient } from "xero-node";
+import { XeroClient, Invoice, RequestEmpty } from "xero-node";
 
 const xero = new XeroClient({
   clientId: process.env.XERO_CLIENT_ID as string,
@@ -24,33 +24,66 @@ export const xeroRouter = router({
       daysTillExpiry: daysTillExpiry,
     };
   }),
-  getAccessToken: adminProcedure.query(async ({ ctx }) => {
-    const token = await ctx.prisma.xeroCreds.findFirst();
-    const clientId = process.env.XERO_CLIENT_ID;
-    const clientSecret = process.env.XERO_CLIENT_SECRET;
-    const res = await fetch("https://identity.xero.com/connect/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        grant_type: "refresh_token",
-      },
-      body: JSON.stringify({
-        client_id: clientId,
-        client_secret: clientSecret,
-        refresh_token: token?.refreshToken,
-        grant_type: "refresh_token",
-      }),
-    });
-    const data = await res.json();
-    return {
-      accessToken: data.access_token,
-    };
-  }),
+  createInvoice: adminProcedure
+    .input(
+      z.object({
+        email: z.string(),
+        orderNo: z.string(),
+        cart: z.array(
+          z.object({
+            partId: z.string(),
+            title: z.string(),
+            quantity: z.number(),
+            price: z.number(),
+          })
+        ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const tokenSet = await xero.refreshToken();
+      await xero.updateTenants();
+      const activeTenantId = xero.tenants[0].tenantId;
+      const createInvoiceResponse = await xero.accountingApi.createInvoices(
+        activeTenantId,
+        {
+          invoices: [
+            {
+              type: Invoice.TypeEnum.ACCREC,
+              contact: {
+                emailAddress: input.email,
+              },
+              date: new Date().toISOString().split("T")[0],
+              reference: input.orderNo,
+              status: Invoice.StatusEnum.PAID, //can this be done?
+              lineItems: input.cart.map((item) => {
+                return {
+                  description: item.title,
+                  quantity: item.quantity,
+                  unitAmount: item.price,
+                  // accountCode: "200", not sure if this is needed, hope not because don't know what it is
+                  // taxType: once again unsure what this is
+                  lineAmount: item.price * item.quantity,
+                };
+              }),
+            },
+          ],
+        }
+      );
+    //   if (createInvoiceResponse?.body?.invoices) {
+    //     const invoiceId = createInvoiceResponse.body.invoices[0]?.invoiceID;
+    //     const requestEmpty: RequestEmpty = {};
+    //     const emailInvoiceResponse = await xero.accountingApi.emailInvoice(
+    //       activeTenantId,
+    //       invoiceId as string,
+    //       requestEmpty
+    //     );
+    //   }
+        // Unsure how above works, does Xero get the email from the invoice we created to send?
+        return {
+            createInvoiceResponse
+        }
+    }),
   authenticate: adminProcedure.mutation(async ({ ctx }) => {
-    // const clientId = process.env.XERO_CLIENT_ID;
-    // const clientSecret = process.env.XERO_CLIENT_SECRET;
-    // const scopes = process.env.XERO_SCOPES;
-    // const redirectUri = process.env.XERO_REDIRECT_URI;
     let consentUrl = await xero.buildConsentUrl();
     return {
       url: consentUrl,
