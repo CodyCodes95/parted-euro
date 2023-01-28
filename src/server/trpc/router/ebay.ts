d820f19f63eb6a0d2e688a5a75c57e347bfe7898
Cody Partington <codythatsme@gmail.com>
2023-01-29 00:06:16 +1100

    ebay endpoint cleaned up

--- src/server/trpc/router/ebay.ts
+++ src/server/trpc/router/ebay.ts
@@ -3,23 +3,22 @@ import { router, adminProcedure } from "../trpc";
 import eBayApi from "ebay-api";
 import {
   FulfillmentPolicyRequest,
-  EbayOfferDetailsWithKeys,
-  InventoryLocationFull,
 } from "ebay-api/lib/types";
 import {
   CategoryType,
+  Condition,
+  ContentLanguage,
+  CurrencyCode,
+  FormatType,
+  Marketplace,
   MarketplaceId,
-  RegionType,
   TimeDurationUnit,
 } from "ebay-api/lib/enums";
-import { RegionSet } from "ebay-api/lib/types";
-import { Region } from "ebay-api/lib/types";
-import { SellInventoryItem } from "ebay-api/lib/types";
 
 const ebay = eBayApi.fromEnv();
 ebay.config.acceptLanguage = "en-AU";
-ebay.config.contentLanguage = "en-AU" as any;
-ebay.config.marketplaceId = "EBAY_AU" as any;
+ebay.config.contentLanguage = "en-AU" as ContentLanguage;
+ebay.config.marketplaceId = "EBAY_AU" as MarketplaceId;
 
 const request: FulfillmentPolicyRequest = {
   name: "Fulfilmnet Policy",
@@ -65,6 +64,13 @@ export const ebayRouter = router({
     .input(
       z.object({
         title: z.string(),
+        description: z.string(),
+        price: z.number(),
+        partNo: z.string(),
+        condition: z.string(),
+        conditionDescription: z.string(),
+        images: z.array(z.string()),
+        quantity: z.number().default(1),
       })
     )
     .mutation(async ({ ctx, input }) => {
@@ -81,49 +87,57 @@ export const ebayRouter = router({
           },
         });
       });
-      const listing = {
-        model: "TEST",
-        sku: "124158001",
-        marketplaceId: "EBAY_AU",
-        format: "FIXED_PRICE",
-        availableQuantity: 1,
-        categoryId: "30093",
-        listingDescription: "Brand new red bikeeeeeeeee",
-        listingPolicies: {
-          fulfillmentPolicyId: "42821376015",
-          paymentPolicyId: "42821372015",
-          returnPolicyId: "205483749015",
-        },
-        merchantLocationKey: "parted-euro-knox",
-        pricingSummary: {
-          price: {
-            currency: "AUD",
-            value: "1005.00",
-          },
-        },
-        quantityLimitPerBuyer: 1,
-        includeCatalogProductDetails: true,
-      } as EbayOfferDetailsWithKeys;
-      // const fulfillmentPolicy = await ebay.sell.account.createFulfillmentPolicy(
-      //   {
-      //     name: "Fulfilmnet Policy",
-      //     description: "Fulfilmnet Policy",
-      //     marketplaceId: MarketplaceId.EBAY_AU,
-      //     handlingTime: {
-      //       unit: TimeDurationUnit.DAY,
-      //       value: 1,
-      //     },
-      //     categoryTypes: [CategoryType.ALL_EXCLUDING_MOTORS_VEHICLES] as any[],
-      //     shipToLocations: {
-      //       regionIncluded: [RegionType.WORLDWIDE] as any[],
-      //       regionExcluded: [] as any[],
-      //     },
-      //   }
-      // );
       try {
-        // const sellItem = await ebay.sell.inventory.createOffer(listing);
-        // const fulfillmentPolicy = await ebay.sell.fulfillment.getOrders();
-        // const sellItem = await ebay.sell.inventory.getInventoryItems()
+        const createInventoryItem =
+          await ebay.sell.inventory.createOrReplaceInventoryItem(input.partNo, {
+            availability: {
+              shipToLocationAvailability: {
+                quantity: input.quantity,
+              },
+            },
+            condition: input.condition as Condition,
+            product: {
+              title: input.title,
+              description: input.description,
+              aspects: {
+                // Brand: ["BMW"],
+                // Type: ["Helmet/Action"],
+                // "Storage Type": ["Removable"],
+                // "Recording Definition": ["High Definition"],
+                // "Media Format": ["Flash Drive (SSD)"],
+                // "Optical Zoom": ["10x"],
+                // "Model": ["Hero4"],
+              },
+              // brand: "GoPro",
+              mpn: input.partNo,
+              imageUrls: input.images,
+            },
+          });
+        const createOffer = await ebay.sell.inventory.createOffer({
+          // model: "TEST",
+          sku: input.partNo,
+          marketplaceId: "EBAY_AU" as Marketplace,
+          format: "FIXED_PRICE" as FormatType,
+          availableQuantity: input.quantity,
+          categoryId: "30093", //find car part id
+          listingDescription: input.description,
+          listingPolicies: {
+            fulfillmentPolicyId: "42821376015",
+            paymentPolicyId: "42821372015",
+            returnPolicyId: "205483749015",
+          },
+          merchantLocationKey: "parted-euro-knox",
+          pricingSummary: {
+            price: {
+              currency: "AUD" as CurrencyCode,
+              value: input.price.toString(),
+            },
+          },
+          // quantityLimitPerBuyer: 1,
+        });
+        const publishOffer = await ebay.sell.inventory.publishOffer(
+          createOffer.offerId
+        );
         // const inventoryLocation =
         //   await ebay.sell.inventory.createInventoryLocation(
         //     "parted-euro-knox",
@@ -147,185 +161,17 @@ export const ebayRouter = router({
         //   );
         // const inventoryLocation =
         // await ebay.sell.inventory.getInventoryLocations();
-        // const sellItem = await ebay.sell.account.getPaymentPolicies("EBAY_AU")
+        // const policies = await ebay.sell.account.getPaymentPolicies("EBAY_AU")
         // const categoryTreeId = await ebay.commerce.taxonomy.getDefaultCategoryTreeId("EBAY_AU") //logged 15, used in req below
         // const sellItem = await ebay.commerce.taxonomy.getCategorySuggestions("15", "GoPro Hero4 Helmet Cam") logged {categoryId: '30093', categoryName: 'Tripods & Monopods'}. Wondering how often
         // this changes? if not often, will just use a generic car parts category for all reqs. If they change, will have to grab on each fetch
-        // const sellItem = await ebay.sell.inventory.createOrReplaceInventoryItem(
-        //   "124158001",
-        //   {
-        //     availability: {
-        //       shipToLocationAvailability: {
-        //         quantity: 10,
-        //       },
-        //     },
-        //     condition: "NEW",
-        //     product: {
-        //       title: "GoPro Hero4 Helmet Cam",
-        //       description: "New GoPro Hero4 Helmet Cam. Unopened box.",
-        //       aspects: {
-        //         Brand: ["GoPro"],
-        //         Type: ["Helmet/Action"],
-        //         "Storage Type": ["Removable"],
-        //         "Recording Definition": ["High Definition"],
-        //         "Media Format": ["Flash Drive (SSD)"],
-        //         "Optical Zoom": ["10x"],
-        //         "Model": ["Hero4"],
-        //       },
-        //       brand: "GoPro",
-        //       mpn: "CHDHX-401",
-        //       imageUrls: [
-        //         "https://res.cloudinary.com/codycodes/image/upload/v1673508756/listings/Photo20-12-2022_60517pm_ozi5cs.webp",
-        //       ],
-        //     },
-        //   } as SellInventoryItem
-        // );
-        // const sellItem = await ebay.sell.inventory.getInventoryItems();
-        // const fulfillmentPolicy =
-        // await ebay.sell.account.createFulfillmentPolicy(request);
-        // const offers = await ebay.sell.inventory.getOffers({
-        //   sku: "12345543219922222",
-        //   marketplaceId: "EBAY_AU",
-        // });
-        const publishOffer = await ebay.sell.inventory.publishOffer(
-          "296734126016"
-        );
         return {
-          // sellItem,
-          // offers,
           publishOffer,
-          // inventoryLocation,
         };
       } catch (err) {
         return {
           err,
         };
       }
-      // const ebayCreds = await ctx.prisma.ebayCreds.findFirst();
-      // const ebayTokenRes = await ebayAuthToken.getAccessToken(
-      //   "PRODUCTION",
-      //   ebayCreds?.refreshToken,
-      //   process.env.EBAY_SCOPES?.split(" ")
-      // );
-      // const tokenSet = JSON.parse(ebayTokenRes as any);
-      // create the listing::::
-      // const res = await fetch(
-      //   `https://api.ebay.com/sell/inventory/v1/inventory_item/11121702856`,
-      //   {
-      //     method: "PUT",
-      //     headers: {
-      //       Authorization: `Bearer ${tokenSet.access_token}`,
-      //       "Content-Type": "application/json",
-      //       "Content-Language": "en-US",
-      //     },
-      //     body: JSON.stringify({
-      //       availability: {
-      //         shipToLocationAvailability: {
-      //           quantity: 50,
-      //         },
-      //       },
-      //       condition: "NEW",
-      //       product: {
-      //         title: "GoPro Hero4 Helmet Cam",
-      //         description: "New GoPro Hero4 Helmet Cam. Unopened box.",
-      //         aspects: {
-      //           Brand: ["GoPro"],
-      //           Type: ["Helmet/Action"],
-      //           "Storage Type": ["Removable"],
-      //           "Recording Definition": ["High Definition"],
-      //           "Media Format": ["Flash Drive (SSD)"],
-      //           "Optical Zoom": ["10x"],
-      //         },
-      //         brand: "GoPro",
-      //         mpn: "CHDHX-401",
-      //         imageUrls: [
-      //           "https://res.cloudinary.com/codycodes/image/upload/v1673508756/listings/Photo20-12-2022_60517pm_ozi5cs.webp",
-      //         ],
-      //       },
-      //     }),
-      //   }
-      // );
-      // const res = await fetch(
-      //   `https://api.ebay.com/sell/inventory/v1/inventory_item?limit=10&offset=0`,
-      //   {
-      //     method: "GET",
-      //     headers: {
-      //       Authorization: `Bearer ${tokenSet.access_token}`,
-      //       "Content-Type": "application/json",
-      //     },
-      //   }
-      // );
-      // const res = await fetch("https://api.ebay.com/sell/inventory/v1/offer", {
-      //   method: "POST",
-      //   headers: {
-      //     Authorization: `Bearer ${tokenSet.access_token}`,
-      //     "Content-Type": "application/json",
-      //     "Content-Language": "en-US",
-      //   },
-      //   body: JSON.stringify({
-      //     sku: "11121702856",
-      //     marketplaceId: "EBAY_AU",
-      //     format: "FIXED_PRICE",
-      //     availableQuantity: 5,
-      //     categoryId: "30120",
-      //     listingDescription:
-      //       "Go pro with great camera great condition and great price",
-      //     listingPolicies: {
-      //       fulfillmentPolicyId: "3*********0",
-      //       paymentPolicyId: "3*********0",
-      //       returnPolicyId: "3*********0",
-      //     },
-      //     pricingSummary: {
-      //       price: {
-      //         currency: "AUD",
-      //         value: "15.75",
-      //       },
-      //     },
-      //     quantityLimitPerBuyer: 2,
-      //     includeCatalogProductDetails: true,
-      //   }),
-      // });
-      // const res = await fetch(
-      //   "https://api.ebay.com/sell/account/v1/fulfillment_policy",
-      //   {
-      //     method: "POST",
-      //     headers: {
-      //       Authorization: `Bearer ${tokenSet.access_token}`,
-      //       "Content-Type": "application/json",
-      //     },
-      //     body: JSON.stringify({
-      //       categoryTypes: [
-      //         {
-      //           name: "ALL_EXCLUDING_MOTORS_VEHICLES",
-      //         },
-      //       ],
-      //       marketplaceId: "EBAY_AU",
-      //       name: "partedeuro",
-      //       handlingTime: {
-      //         unit: "DAY",
-      //         value: "1",
-      //       },
-      //       shippingOptions: [
-      //         {
-      //           costType: "FLAT_RATE",
-      //           optionType: "DOMESTIC",
-      //           shippingServices: [
-      //             {
-      //               buyerResponsibleForShipping: "false",
-      //               freeShipping: "true",
-      //               shippingCarrierCode: "USPS",
-      //               shippingServiceCode: "USPSPriorityFlatRateBox",
-      //             },
-      //           ],
-      //         },
-      //       ],
-      //     }),
-      //   }
-      // );
-      // const data = await res.json();
-      // console.log(data);
-      // return {
-      //   data,
-      // };
     }),
 });
