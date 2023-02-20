f949614d3cfe07e7d59696e64fdb0f10c664b6b9
Cody Partington <codythatsme@gmail.com>
2023-02-21 01:39:52 +1100

    compressor tweaking

--- src/components/listings/AddListing.tsx
+++ src/components/listings/AddListing.tsx
@@ -78,6 +78,8 @@ const AddListing: React.FC<AddListingProps> = ({
       };
       new Compressor(file, {
         quality: 0.6,
+        maxHeight: 711,
+        maxWidth: 400,
         success(result) {
           reader.readAsDataURL(result);
         },
