54f1704229948f5da4d1416d5d0080c929086915
Cody Partington <codythatsme@gmail.com>
2023-03-05 22:28:28 +1100

    refetch added to listings

--- src/pages/admin/listings/index.tsx
+++ src/pages/admin/listings/index.tsx
@@ -95,6 +95,7 @@ const Listings: NextPage = () => {
           <button
             onClick={() => {
               const res = deleteListing.mutateAsync({ id: d.id });
+              listings.refetch()
               success("Listing deleted");
             }}
             className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
