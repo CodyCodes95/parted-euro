7520ba327143670f9881442f86efcfc66cc9c488
Cody Partington <codythatsme@gmail.com>
2023-03-15 23:18:39 +1100

    weight was not added to edit modal

--- src/components/parts/EditPartDetails.tsx
+++ src/components/parts/EditPartDetails.tsx
@@ -178,6 +178,17 @@ const EditPartDetails: React.FC<EditPartProps> = ({
             </div>
             <div className="mb-6">
               <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                Weight
+              </label>
+              <input
+                value={weight || undefined}
+                onChange={(e) => setWeight(Number(e.target.value))}
+                className={` block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500
+              dark:focus:ring-blue-500`}
+              />
+            </div>
+            <div className="mb-6">
+              <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
                 Length
               </label>
               <input
