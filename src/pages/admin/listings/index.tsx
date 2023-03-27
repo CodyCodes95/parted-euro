353396951e12b93c0926787af5652c2b36805d59
Cody Partington <codythatsme@gmail.com>
2023-03-27 23:29:46 +1100

    listed on ebay now showing correctly

--- src/pages/admin/listings/index.tsx
+++ src/pages/admin/listings/index.tsx
@@ -55,10 +55,11 @@ const Listings: NextPage = () => {
       {
         Header: "Listed On Ebay",
         accessor: (d) =>
-          d.ebayListing ? (
-            <button className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
-              View Ebay Listing
-            </button>
+          d.listedOnEbay ? (
+            // <button className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            //   View Ebay Listing
+            // </button>
+          <p>Listed</p>
           ) : (
             <button
               onClick={() => {
