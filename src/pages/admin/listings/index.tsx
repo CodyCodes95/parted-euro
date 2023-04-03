c6b9e2ef5d1445d3103ab52b07565c90beccc97a
Cody Partington <codythatsme@gmail.com>
2023-04-04 00:41:50 +1000

    relist fixed

--- src/pages/admin/listings/index.tsx
+++ src/pages/admin/listings/index.tsx
@@ -58,7 +58,7 @@ const Listings: NextPage = () => {
         accessor: (d) =>
           d.listedOnEbay ? (
             <button onClick={() => {
-              markAsNotListedEbay.mutate(d.id)
+              markAsNotListedEbay.mutate({id: d.id})
               listings.refetch()
             }} className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
               Relist
