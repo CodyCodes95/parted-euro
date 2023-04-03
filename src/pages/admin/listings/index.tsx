ffd6d24f86046819371c293b3bc36abddaf0afc1
Cody Partington <codythatsme@gmail.com>
2023-04-04 00:58:37 +1000

    better workflow when selecting relist

--- src/pages/admin/listings/index.tsx
+++ src/pages/admin/listings/index.tsx
@@ -59,7 +59,8 @@ const Listings: NextPage = () => {
           d.listedOnEbay ? (
             <button onClick={() => {
               markAsNotListedEbay.mutate({id: d.id})
-              listings.refetch()
+              setShowEbayModal(true);
+              setSelected(d);
             }} className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
               Relist
             </button>
