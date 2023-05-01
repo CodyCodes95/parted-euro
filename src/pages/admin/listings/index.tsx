a095d55e4a8b31eb40aa905b416e6b028f39b15d
Cody Partington <codythatsme@gmail.com>
2023-05-02 00:26:57 +1000

    qty on admin table

--- src/pages/admin/listings/index.tsx
+++ src/pages/admin/listings/index.tsx
@@ -50,6 +50,10 @@ const Listings: NextPage = () => {
         ),
       },
       {
+        Header: "Quantity",
+        accessor: "quantity",
+      },
+      {
         Header: "Price",
         accessor: "price",
       },
@@ -57,11 +61,14 @@ const Listings: NextPage = () => {
         Header: "Listed On Ebay",
         accessor: (d) =>
           d.listedOnEbay ? (
-            <button onClick={() => {
-              markAsNotListedEbay.mutate({id: d.id})
-              setShowEbayModal(true);
-              setSelected(d);
-            }} className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+            <button
+              onClick={() => {
+                markAsNotListedEbay.mutate({ id: d.id });
+                setShowEbayModal(true);
+                setSelected(d);
+              }}
+              className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            >
               Relist
             </button>
           ) : (
@@ -100,7 +107,7 @@ const Listings: NextPage = () => {
           <button
             onClick={() => {
               const res = deleteListing.mutateAsync({ id: d.id });
-              listings.refetch()
+              listings.refetch();
               success("Listing deleted");
             }}
             className="mr-2 mb-2 rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
