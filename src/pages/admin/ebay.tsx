f46072df4dd76d0b2ce6ef5e18baba510d272228
Cody Partington <codythatsme@gmail.com>
2023-05-05 00:31:32 +1000

    publish offer field

--- src/pages/admin/ebay.tsx
+++ src/pages/admin/ebay.tsx
@@ -19,12 +19,16 @@ const Ebay: NextPage = () => {
   const getReturn = trpc.ebay.getReturnPolicy.useMutation();
   const getMerchant = trpc.ebay.createInventoryLocation.useMutation();
   const getFulfillment = trpc.ebay.getFulfillmentPolicies.useQuery();
-  const getOffer = trpc.ebay.getOffer.useQuery({
-    id: offer,
-  }, {
-    enabled: false,
-    onSuccess: (data) => console.log(data)
-  });
+  const publishOffer = trpc.ebay.publishOffer.useMutation();
+  const getOffer = trpc.ebay.getOffer.useQuery(
+    {
+      id: offer,
+    },
+    {
+      enabled: false,
+      onSuccess: (data) => console.log(data),
+    }
+  );
   const getOffers = trpc.ebay.getOffers.useQuery(
     {
       sku: sku,
@@ -56,7 +60,7 @@ const Ebay: NextPage = () => {
 
   const getShippingPolicies = async () => {
     // const res = await getFulfillment.mutateAsync();
-   console.log("1")
+    console.log("1");
   };
 
   return (
@@ -98,6 +102,17 @@ const Ebay: NextPage = () => {
           className="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Get Offer
+        </button>
+        <button
+          onClick={async() => {
+            const res = await publishOffer.mutateAsync({
+              offerId: offer,
+            });
+            console.log(res)
+          }}
+          className="mr-2 mb-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-blue-800"
+        >
+          PUBLISH Offer
         </button>
       </div>
       <div>
