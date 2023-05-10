224eed27b12e14c4551d40145c538bcb9fad93f0
Cody Partington <codythatsme@gmail.com>
2023-05-10 23:27:26 +1000

    image sorter for new listings implemented

--- src/components/listings/AddListing.tsx
+++ src/components/listings/AddListing.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { trpc } from "../../utils/trpc";
 import ModalBackDrop from "../modals/ModalBackdrop";
 import Select from "react-select";
@@ -9,8 +9,9 @@ import Compressor from "compressorjs";
 import type { Image, Listing, Part, PartDetail } from "@prisma/client";
 import SortableList, { SortableItem } from "react-easy-sort";
 import { RxCross2 } from "react-icons/rx";
-import { Input } from "../ui/input"; 
+import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
+import ImageSorter from "./ImageSorter";
 
 interface AddListingProps {
   showModal: boolean;
@@ -100,15 +101,6 @@ const AddListing: React.FC<AddListingProps> = ({
         },
       });
     });
-  };
-
-  const onSortEnd = (oldIndex: number, newIndex: number) => {
-    const newImages = [...uploadedImages] as Image[];
-    const [removedImage] = newImages.splice(oldIndex, 1);
-    if (removedImage !== undefined) {
-      newImages.splice(newIndex, 0, removedImage);
-      setUploadedImages(newImages);
-    }
   };
 
   const runUpdateImageOrder = async () => {
@@ -208,68 +200,29 @@ const AddListing: React.FC<AddListingProps> = ({
     setImages([]);
   };
 
-  if (showImageSorter) {
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  if (showImageSorter && listing) {
     return (
-      <div
-        id="defaultModal"
-        aria-hidden="true"
-        className={`h-modal fixed top-0 left-0 right-0 z-50 flex w-full items-center justify-center overflow-y-auto overflow-x-hidden p-4 md:inset-0 md:h-full ${
-          showModal ? "" : "hidden"
-        }`}
-      >
-        <ModalBackDrop setShowModal={setShowModal} />
-        <div className="relative h-full w-full max-w-4xl md:h-auto">
-          <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
-            <div className="flex items-start justify-between rounded-t border-b p-4 dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                Sort Images
-              </h3>
-              <button
-                onClick={() => setShowModal(!showModal) as any}
-                type="button"
-                className="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-toggle="defaultModal"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-            </div>
-            <SortableList
-              className="flex flex-wrap items-center"
-              onSortEnd={onSortEnd}
-              draggedItemClassName=""
-            >
-              {uploadedImages.map((image) => (
-                <SortableItem key={image.url}>
-                  <div className="relative">
-                    <RxCross2
-                      onClick={() => onImageDelete(image)}
-                      className="absolute right-0 cursor-pointer text-xl text-red-500"
-                    />
-                    <img className="h-56 w-32" src={image.url} />
-                  </div>
-                </SortableItem>
-              ))}
-            </SortableList>
-            <button
-              className="mr-2 mt-4 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              onClick={() => {
-                runUpdateImageOrder();
-              }}
-            >
-              Save Order
-            </button>
-          </div>
-        </div>
-      </div>
+      <ImageSorter
+        setImages={setUploadedImages}
+        images={uploadedImages}
+        setShowModal={setShowImageSorter}
+        showModal={showImageSorter}
+      />
+    );
+  }
+
+  if (showImageSorter && !listing) {
+    return (
+      <ImageSorter
+        setImages={setImages}
+        images={images}
+        setShowModal={setShowImageSorter}
+        showModal={showImageSorter}
+      />
     );
   }
 
@@ -377,7 +330,7 @@ const AddListing: React.FC<AddListingProps> = ({
                 />
                 <PhotoCamera />
               </IconButton>
-              <p>{images.length} Photos attached</p>
+              <p>{uploadedImages.length || images.length} Photos attached</p>
               <a
                 onClick={() => setShowImageSorter(true)}
                 className="cursor-pointer text-blue-500"
