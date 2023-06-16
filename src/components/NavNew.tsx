46842c45705108d7d7c7eba11013206bf7a4587a
Cody Partington <codythatsme@gmail.com>
2023-06-17 01:07:05 +1000

    generation nav menu started

--- src/components/NavNew.tsx
+++ src/components/NavNew.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import * as React from "react";
 import Link from "next/link";
 import logo from "../../public/logo.png";
 import { cn } from "../lib/utils";
@@ -14,64 +11,34 @@ import {
   navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 import { useSession, signIn } from "next-auth/react";
-import { Menu, Transition } from '@headlessui/react'
 import { Badge } from "./ui/badge";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import CartPopover from "./Nav/CartPopover";
 import AdminMenu from "./Nav/AdminMenu";
 import SearchBar from "./Nav/SearchBar";
-import {BsFillPersonFill} from "react-icons/bs";
-import {FiLogIn} from "react-icons/fi";
+import { FiLogIn } from "react-icons/fi";
 import CartContext from "../context/cartContext";
 import { FaShoppingCart, FaSearch } from "react-icons/fa";
+import { forwardRef, useContext, useRef, useState } from "react";
 
-
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
+const generations = [
+  "F8X - M2/M3/M4",
+  "E36 - 3 Series",
+  "E46 - 3 Series",
+  "E34 - 5 Series",
+  "E39 - 5 Series",
+  "E38 - 7 Series",
+  "E31 - 8 Series",
+  "E53 - X Series",
 ];
 
 const NavNew = () => {
-  const { cart, setCart } = React.useContext(CartContext);
-  const [showLogin, setShowLogin] = React.useState<boolean>(false);
-  const [showSearch, setShowSearch] = React.useState<boolean>(false);
-  const [openAdminMenu, setOpenAdminMenu] = React.useState<boolean>(false);
+  const { cart, setCart } = useContext(CartContext);
+  const [showLogin, setShowLogin] = useState<boolean>(false);
+  const [showSearch, setShowSearch] = useState<boolean>(false);
+  const [openAdminMenu, setOpenAdminMenu] = useState<boolean>(false);
 
-  const adminRef = React.useRef<HTMLDivElement>(null);
+  const adminRef = useRef<HTMLDivElement>(null);
   const { data: session } = useSession();
   return (
     <div className="flex h-16 w-full items-center justify-between border-b-2 bg-white px-16 py-8">
@@ -92,33 +59,35 @@ const NavNew = () => {
           <NavigationMenuItem>
             <NavigationMenuTrigger>Shop By Generation</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <Link
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      {/* <Icons.logo className="h-6 w-6" /> */}
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        shadcn/ui
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        Beautifully designed components built with Radix UI and
-                        Tailwind CSS.
-                      </p>
-                    </Link>
-                  </NavigationMenuLink>
-                </li>
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
+              <ul className="grid w-screen gap-3 p-6 lg:grid-cols-6">
+                {generations.map((generation) => {
+                  return (
+                    <li className="flex flex-col" key={generation}>
+                      <NavigationMenuLink asChild>
+                        <a
+                          className={cn(
+                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                          )}
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            All {generation} Parts
+                          </div>
+                        </a>
+                      </NavigationMenuLink>
+                      <NavigationMenuLink asChild>
+                        <a
+                          className={cn(
+                            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+                          )}
+                        >
+                          <div className="text-sm font-medium leading-none">
+                            All {generation} Parts
+                          </div>
+                        </a>
+                      </NavigationMenuLink>
+                    </li>
+                  );
+                })}
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
@@ -187,9 +156,9 @@ const NavNew = () => {
           </PopoverContent>
         </Popover>
         <div
-          className={`p-2 ${
-            !session && !showLogin ? "invisible" : ""
-          } ${showLogin && !session ? "visible" : ""}`}
+          className={`p-2 ${!session && !showLogin ? "invisible" : ""} ${
+            showLogin && !session ? "visible" : ""
+          }`}
         >
           {session ? (
             <AdminMenu />
@@ -201,9 +170,9 @@ const NavNew = () => {
       <SearchBar showSearch={showSearch} setShowSearch={setShowSearch} />
     </div>
   );
-}
+};
 
-const ListItem = React.forwardRef<
+const ListItem = forwardRef<
   React.ElementRef<"a">,
   React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
