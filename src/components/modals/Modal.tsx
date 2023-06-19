16007a3a2e69781d851ffdf2c987405f88b26e6d
Cody Partington <codythatsme@gmail.com>
2023-06-19 23:46:31 +1000

    small modal style changes

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -34,9 +34,10 @@ const Modal: FC<ModalProps> = ({
       {/* Full-screen container to center the panel */}
       <div className="fixed inset-0 flex items-center justify-center p-4">
         {/* The actual dialog panel  */}
-        <Dialog.Panel className="mx-auto w-1/2 rounded-lg bg-white">
-          <div className="flex w-full items-center justify-between border-b-2 p-2">
-            <Dialog.Title className="p-2 text-xl font-medium">
+        {/* <Dialog.Panel className="mx-auto w-1/2 rounded-lg bg-white p-2 max-h-[45rem] overflow-y-scroll"> */}
+        <Dialog.Panel className="mx-auto w-1/2 rounded-lg bg-white p-2">
+          <div className="flex w-full items-center justify-between border-b-2 p-2 rounded-t-lg">
+            <Dialog.Title className="text-xl font-medium">
               {title}
             </Dialog.Title>
             <X
