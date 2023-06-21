a1d377a94af0749b2598dc9a8d6e865a5ffe543a
Cody Partington <codythatsme@gmail.com>
2023-06-21 22:53:48 +1000

    schema update

--- prisma/dbScripts.ts
+++ prisma/dbScripts.ts
@@ -1,43 +1,46 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
-async function migrate() {
-  const parentCategories = await prisma.partTypeParentCategory.findMany();
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
-  // Create a new PartType for each PartTypeParentCategory
-  for (const category of parentCategories) {
-    const newPartType = await prisma.partTypes.create({
-      data: {
-        id: category.id, // Use the same ID to keep references intact
-        name: category.name,
-        createdAt: category.createdAt,
-        updatedAt: category.updatedAt,
-      },
-    });
+// async function migrate() {
+//   const parentCategories = await prisma.partTypeParentCategory.findMany();
 
-    console.log(`Created new PartType: ${newPartType.name}`);
-  }
+//   // Create a new PartType for each PartTypeParentCategory
+//   for (const category of parentCategories) {
+//     const newPartType = await prisma.partTypes.create({
+//       data: {
+//         id: category.id, // Use the same ID to keep references intact
+//         name: category.name,
+//         createdAt: category.createdAt,
+//         updatedAt: category.updatedAt,
+//       },
+//     });
 
-  // Update all PartTypes to have the correct parentId
-  const partTypes = await prisma.partTypes.findMany();
-  for (const partType of partTypes) {
-    if (partType.parentId) {
-      await prisma.partTypes.update({
-        where: { id: partType.id },
-        data: { parentId: partType.parentId },
-      });
-      console.log(`Updated PartType: ${partType.name}`);
-    }
-  }
+//     console.log(`Created new PartType: ${newPartType.name}`);
+//   }
 
-  console.log("Migration completed");
-}
+//   // Update all PartTypes to have the correct parentId
+//   const partTypes = await prisma.partTypes.findMany();
+//   for (const partType of partTypes) {
+//     if (partType.parentId) {
+//       await prisma.partTypes.update({
+//         where: { id: partType.id },
+//         data: { parentId: partType.parentId },
+//       });
+//       console.log(`Updated PartType: ${partType.name}`);
+//     }
+//   }
 
-migrate()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+//   console.log("Migration completed");
+// }
+
+// migrate()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
+
+console.log("test")
