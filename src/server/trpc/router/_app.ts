ad6ad6cbad909ae18f69bd1c7c7f193a11d39a59
Cody Partington <codythatsme@gmail.com>
2023-06-21 22:58:55 +1000

    example api files removed

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,7 +2,6 @@ import { donorRouter } from "./donors";
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { carRouter } from "./car";
-import { exampleRouter } from "./example";
 import { listingRouter } from "./listings";
 import { partRouter } from "./parts";
 import { imagesRouter } from "./images";
@@ -13,7 +12,6 @@ import { inventoryLocationRouter } from "./inventoryLocations";
 import { categoryRouter } from "./categories";
 
 export const appRouter = router({
-  example: exampleRouter,
   cars: carRouter,
   donors: donorRouter,
   listings: listingRouter,
