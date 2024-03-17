7b58ec83c28fca27b3d2446d21939d1785b1ecc4
Cody Partington <codythatsme@gmail.com>
2024-03-17 21:06:57 +1100

    Add order sorting by createdAt in getAllAdmin

--- src/server/trpc/router/order.ts
+++ src/server/trpc/router/order.ts
@@ -59,6 +59,9 @@ export const orderRouter = router({
     }),
   getAllAdmin: adminProcedure.query(({ ctx }) => {
     return ctx.prisma.order.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
       include: {
         orderItems: {
           include: {
