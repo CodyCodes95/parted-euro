f668475878614f57b50139da96acac278fbd52b5
Cody Partington <codythatsme@gmail.com>
2024-07-01 20:02:42 +1000

    unused imports removed

--- src/components/listings/EbayModal.tsx
+++ src/components/listings/EbayModal.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState, Fragment } from "react";
 import type { QueryListingsGetAllAdmin } from "../../utils/trpc";
 import { trpc } from "../../utils/trpc";
 import Select from "react-select";
-import { Combobox, Transition } from "@headlessui/react";
-import { BiChevronDown } from "react-icons/bi";
 import type { Car, Part, PartDetail } from "@prisma/client";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
@@ -290,73 +288,6 @@ const EbayModal: React.FC<EbayModalProps> = ({
           </>
         ) : (
           <>
-            {/* <Combobox value={fulfillmentPolicy} onChange={setFulfillmentPolicy}>
-              <div className="relative mt-1">
-                <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                  <Combobox.Input
-                    className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                    displayValue={() =>
-                      fulfillmentPolicy?.name || "Select a fulfillment policy"
-                    }
-                  />
-                  <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                    <BiChevronDown
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </Combobox.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  leave="transition ease-in duration-100"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                    {fulfillmentPolicies.data?.length === 0 ? (
-                      <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                        Nothing found.
-                      </div>
-                    ) : (
-                      fulfillmentPolicies.data?.map(
-                        (policy: FulfillmentPolicyType) => (
-                          <Combobox.Option
-                            key={policy.fulfillmentPolicyId}
-                            className={({ active }) =>
-                              `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                active
-                                  ? "bg-teal-600 text-white"
-                                  : "text-gray-900"
-                              }`
-                            }
-                            value={policy}
-                          >
-                            {({ selected, active }) => (
-                              <>
-                                <span
-                                  className={`block truncate ${
-                                    selected ? "font-medium" : "font-normal"
-                                  }`}
-                                >
-                                  {policy.name}
-                                </span>
-                                {selected ? (
-                                  <span
-                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                      active ? "text-white" : "text-teal-600"
-                                    }`}
-                                  ></span>
-                                ) : null}
-                              </>
-                            )}
-                          </Combobox.Option>
-                        )
-                      )
-                    )}
-                  </Combobox.Options>
-                </Transition>
-              </div>
-            </Combobox> */}
             <Select
               placeholder="Select a fulfillment policy"
               value={{
