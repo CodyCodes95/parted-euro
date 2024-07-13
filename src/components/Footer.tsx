807c02f556e9a0aa9a446a7251a3e8722fe4abdc
Cody Partington <codythatsme@gmail.com>
2024-07-13 12:03:34 +1000

    footer link styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,8 +12,12 @@ const Footer: React.FC = () => {
           </h4>
           <div className="p-2 text-xl"></div>
           <p>Feel free to call or email us at:</p>
-          <p>Mobile: <a href="tel:0431133764">0431 133 764</a></p>
-          <p>Email: <a href="mailto:contact@partedeuro.com.au">contact@partedeuro.com.au</a></p>
+          <p>Mobile: <a
+            className="text-blue-500"
+            href="tel:0431133764">0431 133 764</a></p>
+          <p>Email: <a
+            className="text-blue-500"
+            href="mailto:contact@partedeuro.com.au">contact@partedeuro.com.au</a></p>
         </div>
         <div className="text-md flex w-full flex-col p-4 text-[#4d4d4d] md:w-[50%]">
           <p className="p-1">
