0abc6d4af862a229340ce67e2bd7eaafc1d9b68e
Cody Partington <codythatsme@gmail.com>
2025-02-12 23:51:04 +1100

    nav height fix

--- src/components/Nav/CartPopover.tsx
+++ src/components/Nav/CartPopover.tsx
@@ -59,7 +59,7 @@ const CartPopover = () => {
           )}
         </Button>
       </DrawerTrigger>
-      <DrawerContent className="z-[100] h-[96vh]">
+      <DrawerContent className="z-[100] h-[100dvh]">
         <DrawerHeader className="border-b px-4 py-3">
           <DrawerTitle>Shopping Cart</DrawerTitle>
         </DrawerHeader>
